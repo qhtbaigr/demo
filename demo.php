a97736a96d6beee132127f97c2556c047cb984b9
qhtbaigr <422673451@qq.com>
2018-02-22 17:57:35 +0800

    222

--- demo.php
+++ demo.php
@@ -1,21 +1,2 @@
 <?php
-/**
- *
- */
-class dealNumber
-{
-
-    public function dealNum($number)
-    {
-        if ($number > 1) {
-            return $number * $this->dealNum($number - 1);
-        } else {
-            return true;
-        }
-        return $number > 1 ? $this->dealNum($number - 1) * $number : 1; //三元运算递归 阶乘
-        return array_product(range(1, $n)); //数组乘积
-    }
-}
-$result = new dealNumber();
-$res    = $result->dealNum(9);
-print_r($res);die;
+echo "string";
